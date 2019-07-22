68bb3228236366e77b053965b7fdfdcc8114fc68
Robert Rongen <robert.rongen@blockchainadvies.nu>
2019-07-22 12:16:28 +0200

    Update SimpleStorage.sol

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,7 +1,20 @@
+//Tell the Solidity compiler what version to use
+
 pragma solidity ^0.5.0;
+// pragma solidity ^0.4.8;
 
-
+//Declares a new contract
 contract SimpleStorage {
-  constructor() public {
-  }
+    //Storage. Persists in between transactions
+    uint x;
+
+    //Allows the unsigned integer stored to be changed
+    function set(uint newValue) {
+        x = newValue;
+    }
+    
+    //Returns the currently stored unsigned integer
+    function get() returns (uint) {
+        return x;
+    }
 }
